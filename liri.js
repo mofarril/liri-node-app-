70c747b985032e639a718239be65091373f3f6d1
Madison Ofarril <mofarril.mof@gmail.com>
2019-09-25 15:55:44 -0400

    liri is now responding to something

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@
 //reads & sets environment vars
 require("dotenv").config();
 //import axios NPM
-var axios = require("axios");
+var axios = require("./node_modules/axios");
 //import spotify NPM
 let Spotify = require("node-spotify-api");
 //import fs package
@@ -129,91 +129,4 @@ var runLiri = function (argOne, argTwo) {
 runLiri(process.argv[2], process.argv[3]);
 
 
-//commented this stuff out and started over
-/*default movie Mr. Nobody
-
-if (args[0] === "movie-this") {
-    if (args[1] === undefined) {
-        gotMovie("Mr.+Nobody");
-    }
-    else {
-        getMovie(args.slice(1).join("+"));
-    }
-};
-//spotify this song, default "The Sign"
-if (args[0] === "spotify-this-song") {
-
-    if (args[1] == undefined) {
-        spotifySong("The Sign");
-    }
-    else {
-        let songTitle = args.slice(1).join(" ");
-        spotifySong(songTitle);
-    }
-};
-
-// reads from random.txt
-
-if (args[0] === "do-what-it-says") {
-    fs.readFile("random.txt", "utf8", function (error, data) {
-        if (error) {
-            return console.log(error);
-        }
-        dataArr = data.split(",");
-        if (dataArr[0] === "movie-this") {
-            if (dataArr[1] === undefined) {
-                getMovie("Mr.+Nobody")
-            }
-            else {
-                getMovie(dataArr[1].split().join("+"))
-            }
-        };
-        if (dataArr[0] === "spotify-this-song") {
-            if (dataArr[1] === undefined) {
-                spotifySong("The Sign")
-            } else {
-                spotifySong(dataArr[1])
-            }
-        };
-
-    })
-}
-function spotifySong(songName) {
-
-    spotify.search(({ type: 'track', query: songName, limit: 5 }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        data.tracks.items.forEach(function (element) {
-            console.log("");
-            console.log(`Artist: ${element.artists[0].name}`);
-            console.log(`Song: ${songName}`);
-            console.log(`Spotify Preview Link:${element.preview_url}`);
-            console.log(`Album: ${element.album.name}`);
-        });
-    }))
-}
-
-
-//movie title through omdb api to get more info
-
-function getMovie(movieName) {
-
-    axios
-        .get(`http://www.omdbapi.com/?t=${movieName}&apikey=7af38222`)
-        .then(function (movie) {
-
-            console.log("");
-            console.log(`Title:${movie.data.Title}`);
-            console.log(`Realeased: ${movie.data.year}`);
-            console.log(`IMDB Rating: ${movie.data.Ratings[1].Value}`);
-            console.log(`Rotten Tomatoes Rating: ${movie.data.Ratings[1].Value}`);
-            console.log(`Produced in: ${movie.data.Country}`);
-            console.log(`Plot: ${movie.data.Plot}`);
-            console.log(`Starring: ${movie.data.Actors}`);
-
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
-};
+ 
