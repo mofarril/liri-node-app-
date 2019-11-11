9d7ab8004ceeb87e29d7b0af5f5244befe311ae0
Madison Ofarril <mofarril.mof@gmail.com>
2019-11-11 17:55:17 -0500

    everything is working except do-what-it-says

---
+++ exTrackobjSpotify.js
@@ -0,0 +1,164 @@
+{
+  "tracks": {
+    "href": "https://api.spotify.com/v1/search?query=abba&type=track&market=US&offset=0&limit=2",
+    "items": [
+      {
+        "album": {
+          "album_type": "album",
+          "artists": [
+            {
+              "external_urls": {
+                "spotify": "https://open.spotify.com/artist/0LcJLqbBmaGUft1e9Mm8HV"
+              },
+              "href": "https://api.spotify.com/v1/artists/0LcJLqbBmaGUft1e9Mm8HV",
+              "id": "0LcJLqbBmaGUft1e9Mm8HV",
+              "name": "ABBA",
+              "type": "artist",
+              "uri": "spotify:artist:0LcJLqbBmaGUft1e9Mm8HV"
+            }
+          ],
+          "external_urls": {
+            "spotify": "https://open.spotify.com/album/1M4anG49aEs4YimBdj96Oy"
+          },
+          "href": "https://api.spotify.com/v1/albums/1M4anG49aEs4YimBdj96Oy",
+          "id": "1M4anG49aEs4YimBdj96Oy",
+          "images": [
+            {
+              "height": 575,
+              "url": "https://i.scdn.co/image/7606898c34d84927cb3763459b30de1896e7ecd0",
+              "width": 640
+            },
+            {
+              "height": 270,
+              "url": "https://i.scdn.co/image/e1713bac498e456de997482fa2df8f62a5f5a25d",
+              "width": 300
+            },
+            {
+              "height": 57,
+              "url": "https://i.scdn.co/image/3e4fcd3bdc942c779cc18bf39b8666ce487bf475",
+              "width": 63
+            }
+          ],
+          "name": "Arrival",
+          "release_date": "1976",
+          "release_date_precision": "year",
+          "total_tracks": 12,
+          "type": "album",
+          "uri": "spotify:album:1M4anG49aEs4YimBdj96Oy"
+        },
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/0LcJLqbBmaGUft1e9Mm8HV"
+            },
+            "href": "https://api.spotify.com/v1/artists/0LcJLqbBmaGUft1e9Mm8HV",
+            "id": "0LcJLqbBmaGUft1e9Mm8HV",
+            "name": "ABBA",
+            "type": "artist",
+            "uri": "spotify:artist:0LcJLqbBmaGUft1e9Mm8HV"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 230693,
+        "explicit": false,
+        "external_ids": {
+          "isrc": "SEAYD7601020"
+        },
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/4NtUY5IGzHCaqfZemmAu56"
+        },
+        "href": "https://api.spotify.com/v1/tracks/4NtUY5IGzHCaqfZemmAu56",
+        "id": "4NtUY5IGzHCaqfZemmAu56",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Dancing Queen",
+        "popularity": 73,
+        "preview_url": null,
+        "track_number": 2,
+        "type": "track",
+        "uri": "spotify:track:4NtUY5IGzHCaqfZemmAu56"
+      },
+      {
+        "album": {
+          "album_type": "album",
+          "artists": [
+            {
+              "external_urls": {
+                "spotify": "https://open.spotify.com/artist/0LcJLqbBmaGUft1e9Mm8HV"
+              },
+              "href": "https://api.spotify.com/v1/artists/0LcJLqbBmaGUft1e9Mm8HV",
+              "id": "0LcJLqbBmaGUft1e9Mm8HV",
+              "name": "ABBA",
+              "type": "artist",
+              "uri": "spotify:artist:0LcJLqbBmaGUft1e9Mm8HV"
+            }
+          ],
+          "external_urls": {
+            "spotify": "https://open.spotify.com/album/5GwbPSgiTECzQiE6u7s0ZN"
+          },
+          "href": "https://api.spotify.com/v1/albums/5GwbPSgiTECzQiE6u7s0ZN",
+          "id": "5GwbPSgiTECzQiE6u7s0ZN",
+          "images": [
+            {
+              "height": 575,
+              "url": "https://i.scdn.co/image/bcf257ae27c548c1668660ef362889ce8eb07010",
+              "width": 640
+            },
+            {
+              "height": 270,
+              "url": "https://i.scdn.co/image/4ecb8320cf3eee6343ad758ace5c5521dd4cb32d",
+              "width": 300
+            },
+            {
+              "height": 58,
+              "url": "https://i.scdn.co/image/79657d4b2437a1db6675925f4cd503cfa0660591",
+              "width": 64
+            }
+          ],
+          "name": "The Album",
+          "release_date": "1977",
+          "release_date_precision": "year",
+          "total_tracks": 10,
+          "type": "album",
+          "uri": "spotify:album:5GwbPSgiTECzQiE6u7s0ZN"
+        },
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/0LcJLqbBmaGUft1e9Mm8HV"
+            },
+            "href": "https://api.spotify.com/v1/artists/0LcJLqbBmaGUft1e9Mm8HV",
+            "id": "0LcJLqbBmaGUft1e9Mm8HV",
+            "name": "ABBA",
+            "type": "artist",
+            "uri": "spotify:artist:0LcJLqbBmaGUft1e9Mm8HV"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 243933,
+        "explicit": false,
+        "external_ids": {
+          "isrc": "SEAYD7702020"
+        },
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/6vQN2a9QSgWcm74KEZYfDL"
+        },
+        "href": "https://api.spotify.com/v1/tracks/6vQN2a9QSgWcm74KEZYfDL",
+        "id": "6vQN2a9QSgWcm74KEZYfDL",
+        "is_local": false,
+        "is_playable": true,
+        "name": "Take A Chance On Me",
+        "popularity": 63,
+        "preview_url": null,
+        "track_number": 2,
+        "type": "track",
+        "uri": "spotify:track:6vQN2a9QSgWcm74KEZYfDL"
+      }
+    ],
+    "limit": 2,
+    "next": "https://api.spotify.com/v1/search?query=abba&type=track&market=US&offset=2&limit=2",
+    "offset": 0,
+    "previous": null,
+    "total": 9511
+  }
+}
--- liri.js
+++ liri.js
@@ -3,109 +3,135 @@
 //reads & sets environment vars
 require("dotenv").config();
 //import axios NPM
-var axios = require("./node_modules/axios");
+const axios = require("./node_modules/axios");
 //import spotify NPM
-let Spotify = require("node-spotify-api");
+const Spotify = require("node-spotify-api");
 //import fs package
-let fs = require("fs");
+const fs = require("fs");
 //import API keys
-var keys = require("./keys.js");
+const keys = require("./keys.js");
 // import request moment.js
-var moment= require("moment");
+const moment = require("moment");
 // Init spotify API client using client id and secret
-var spotify = new Spotify(keys.spotify);
+const spotify = new Spotify(keys.spotify);
 
 
 // FUNCTIONS
 
- //idk if this works///
+//idk if this works///
 
-//appends history.txt file
+//appends history.txt file - not working
 
-fs.appendFile("history.txt", options, function(err) {
-    if (err) throw err;
-  });
+//fs.appendFile("history.txt", options, function (err) {
+//     if (err) throw err;
+//     console.log("history.txt was appeneded")
+//   });/
+
+//var getArtistNames = function (artist) {
+  //  return artist.name;
+//};
+
 // Function for Spotify search
-var getSpotify = function (songName) {
+
+let getSpotify = (songName) => {
+
+    //if no song entered the default is:
+
     if (!songName) {
         songName = "The Sign";
     }
+
+    //idk how to say this; something to do with using API keys and spotify module to search
     spotify.search(
         {
             type: "track",
             query: songName
         },
-        function (err, data) {
+        (err, data) => {
             if (err) {
                 console.log("Error occurred: " + err);
                 return;
             }
-            var songs = data.tracks.items;
-            for (var i = 0; i < songs.length; i++) {
-                console.log(i);
-                console.log("artist(s): " + songs[i].artists);
+            // var getArtistNames = function (artist) {
+            //     return artist.name;
+            //     console.log(artist);
+            // };
+            console.log(data.tracks.items[0].album.artists[0].name)
+            let songs = data.tracks.items;
+           // console.log(songs.artists)
+             for (let i = 0; i < songs.length; i++) {
+                
+                 console.log("artist(s): " + songs[i].album.artists[0].name); //not working dunno why!?!
                 console.log("song name: " + songs[i].name);
                 console.log("preview song: " + songs[i].preview_url);
                 console.log("album: " + songs[i].album.name);
                 console.log("-----------------------------------");
-            }
+             }
         }
     );
 };
 // Function for running a BandsInTown Search
-var getTour = function (artist) {
-    
-    var urlHit =
-        "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=3bf94057849bab04d58bc26c6e5ac4d1&date=upcoming";
-           
-        console.log(urlHit);
+let getTour = (artist) => {
+    artist = encodeURI(artist)
+    const urlHit =
+        "https://rest.bandsintown.com/artists/"+artist+"/events?app_id=3bf94057849bab04d58bc26c6e5ac4d1&date=upcoming";
 
-    axios.get(urlHit).then(function (response) {
-           
-    console.log("Venue: " + response.venue); //
-    console.log("Location: " + response.venue.city);// 
-    console.log("Date:" + moment(response.data[1].datatime).format("MM/DD/YYYY"));
+    console.log(urlHit);
+
+    axios.get(urlHit).then((response) => {
+        var concerts = response.data
+        for (let i = 0; i < concerts.length; i++) {
+            console.log("Venue: " + concerts[i].venue.name); //says name is undefined
+            console.log("Location: " + concerts[i].venue.city);// 
+            console.log("Date:" + moment(concerts[i].datatime).format("MM/DD/YYYY"));
+            console.log('===========================================================')
+        }
+
     });
 };
+
 // Function for running a Movie Search
 
-var getMovie = function (movieName) {
+let getMovie = (movieName) => {
 
-    if(!movieName){
-        movieName ="Mr.Nobody";
+    if (!movieName) {
+        movieName = "Mr.Nobody";
     }
-    
+
     var urlHit =
         "http://www.omdbapi.com/?t=" +
         movieName +
         "&y=&plot=full&tomatoes=true&apikey=7af38222";
-       
-        axios.get(urlHit)
-        .then(function (response) {
 
-            var r = response.data
+    axios.get(urlHit)
+        .then((response) => {
+
+            let r = response.data
+
             console.log("Title: " + r.Title);
             console.log("Year: " + r.Year);
             console.log("Rated: " + r.Rated);
             console.log("IMDB Rating: " + r.Ratings[0].Value);
             console.log("Country: " + r.Country);
-            console.log("Language: " + data.Language);
+            console.log("Language: " + r.Language);
             console.log("Plot: " + r.Plot);
             console.log("Actors: " + r.Actors);
             console.log("Rotton Tomatoes Rating: " + r.Ratings[1].Value);
-        } 
-        )};
+        }
+        )
+};
 
 // Function for running a command based on text file
 
-var doWhatItSays = function () {
+let doWhatItSays = () => {
 
-    fs.readFile('random.txt', (err, data) => {
+    fs.readFile('./random.txt', (err, data) => {
         if (err) throw err;
         console.log(data);
-      
 
-        var dataArr = data;
+
+        let dataArr = data
+        console.log(dataArr)
 
         if (dataArr.length === 2) {
             options(dataArr[0], dataArr[1]);
@@ -114,8 +140,8 @@ var doWhatItSays = function () {
         }
     });
 };
-// Function for determining which command is executed
-var options = function (caseData, functionData) {
+// Function for determining which command is executed; calling var options above to print results to history.txt
+let options = (caseData, functionData) => {
     switch (caseData) {
         case "concert-this":
             getTour(functionData);
@@ -134,7 +160,7 @@ var options = function (caseData, functionData) {
     }
 };
 // Function which takes in command line arguments and executes correct function accordigly
-var runLiri = function (argOne, argTwo) {
+const runLiri = (argOne, argTwo) => {
     options(argOne, argTwo);
 };
 // LIRI SHOULD WORK
